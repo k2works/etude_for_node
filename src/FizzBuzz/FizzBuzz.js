a139686bf2cc7fef484d3e741ea54993bbee5d09
k2works <kakimomokuri@gmail.com>
2017-04-15 16:16:07 +0900

    リファクタリング
    アルゴリズムの変更をする。

--- src/FizzBuzz/FizzBuzz.js
+++ src/FizzBuzz/FizzBuzz.js
@@ -3,16 +3,18 @@ class FizzBuzz {
         this.itsNumber = number;
     }
     execute() {
-        let ret = 'Nothing';
-        if (this.itsNumber % 3 === 0) {
-            ret = 'Fizz';
-        }
-        if (this.itsNumber % 5 === 0) {
-            ret = 'Buzz';
-        }
+        let ret;
+
         if (this.itsNumber % 3 === 0 && this.itsNumber % 5 === 0) {
             ret = 'FizzBuzz';
+        } else if (this.itsNumber % 3 === 0) {
+            ret = 'Fizz';
+        } else if (this.itsNumber % 5 === 0) {
+            ret = 'Buzz';
+        } else {
+            ret = 'Nothing';
         }
+
         return ret;
     }
 }
