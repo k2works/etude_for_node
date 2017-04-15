03ade4c6054a1d20722f5d38004739861634745b
k2works <kakimomokuri@gmail.com>
2017-04-15 15:27:47 +0900

    FizzBuzzテストケース作成

--- src/FizzBuzz/FizzBuzz.js
+++ src/FizzBuzz/FizzBuzz.js
@@ -1,3 +1,5 @@
 class FizzBUzz {
-
+    execute() {
+        0
+    }
 }
--- test/FizzBuzz_test.js
+++ test/FizzBuzz_test.js
@@ -2,6 +2,9 @@ import assert from 'power-assert';
 import FizzBUzz from '../src/FizzBuzz/FizzBuzz';
 
 describe('FizzBuzz', () => {
-    it('should do something useful', () => {
+    describe('#execute', () => {
+        it('should return Fizz when it can be divided three');
+        it('should return Buzz when it can be divided five');
+        it('should return FizzBuzz when it can be divided three and five');
     });
 });
