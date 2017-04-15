2404acc20ea751c305beb92ac7ab200c01e3025b
k2works <kakimomokuri@gmail.com>
2017-04-15 20:22:28 +0900

    リファクタリング
    説明変数の導入を実施する。

--- src/GeneratePrimes/GeneratePrimes.js
+++ src/GeneratePrimes/GeneratePrimes.js
@@ -1,32 +1,32 @@
 class GeneratePrimes {
     static generatePrimes(maxValue) {
-        let primes;
-        let s = maxValue + 1; // 配列のサイズ
+        let result;
+        let arraySize = maxValue + 1;
         if (maxValue < 2) {
             return new Array(0);
         } else {
-            let f = this.initializeArray(s);
-            this.sieve(s, f);
-            primes = this.loadPrimes(s, f);
-            return primes; // 素数を返す
+            let f = this.initializeArrayOfInteger(arraySize);
+            this.crossOutMultiples(arraySize, f);
+            result = this.putUncrossedIntegersIntoResult(arraySize, f);
+            return result;
         }
     }
 
-    static initializeArray(s) {
-        let f = Array.apply(null, Array(s)).map(function () {
+    static initializeArrayOfInteger(arraySize) {
+        let f = Array.apply(null, Array(arraySize)).map(function () {
             return true
         });
         f[0] = f[1] = false; // 素数でも倍数でもない
         return f;
     }
 
-    static sieve(s, f) {
+    static crossOutMultiples(arraySize, f) {
         // ふるい落とす
         let i;
         let j;
-        for (i = 2; i < Math.sqrt(s) + 1; i++) {
+        for (i = 2; i < Math.sqrt(arraySize) + 1; i++) {
             if (f[i]) { // iが除かれていなければ、その倍数を除く
-                for (j = 2 * i; j < s; j += i) {
+                for (j = 2 * i; j < arraySize; j += i) {
                     f[j] = false; // 倍数は素数ではない
                 }
             }
@@ -34,12 +34,12 @@ class GeneratePrimes {
         return j;
     }
 
-    static loadPrimes(s, f) {
+    static putUncrossedIntegersIntoResult(arraySize, f) {
         let i;
         let j;
         // 見つけた素数の個数をカウント
         let count = 0;
-        for (i = 0; i < s; i++) {
+        for (i = 0; i < arraySize; i++) {
             if (f[i]) {
                 count++; // カウントアップ
             }
@@ -48,7 +48,7 @@ class GeneratePrimes {
         let primes = new Array(count);
 
         // 素数の抜き出し
-        for (i = 0, j = 0; i < s; i++) {
+        for (i = 0, j = 0; i < arraySize; i++) {
             if (f[i]) { // 素数であれば
                 primes[j++] = i;
             }
