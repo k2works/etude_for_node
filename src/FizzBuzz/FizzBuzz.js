7499e30579b4699737410f23432104645e61b0f1
k2works <kakimomokuri@gmail.com>
2017-04-15 15:44:51 +0900

    FizzBuzzメソッド更新
    ３で割り切れる場合のテストケースを通るようにする。

--- src/FizzBuzz/FizzBuzz.js
+++ src/FizzBuzz/FizzBuzz.js
@@ -1,5 +1,13 @@
 class FizzBuzz {
+    constructor(number) {
+        this.itsNumber = number;
+    }
     execute() {
+        let ret;
+        if (this.itsNumber % 3 === 0) {
+            ret = 'Fizz';
+        }
+        return ret;
     }
 }
 export default FizzBuzz;
--- test/FizzBuzz_test.js
+++ test/FizzBuzz_test.js
@@ -4,7 +4,7 @@ import FizzBuzz from '../src/FizzBuzz/FizzBuzz';
 describe('FizzBuzz', () => {
     describe('#execute', () => {
         it('should return Fizz when it can be divided three', () => {
-            let fb = new FizzBuzz();
+            let fb = new FizzBuzz(3);
             assert(fb.execute() === 'Fizz');
         });
         it('should return Buzz when it can be divided five', () => {
