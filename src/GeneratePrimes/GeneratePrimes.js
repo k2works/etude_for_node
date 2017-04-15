45c824b017478cb3ba8402031488ed4bfbb50366
k2works <kakimomokuri@gmail.com>
2017-04-15 18:18:09 +0900

    素数を作るクラス作成

---
+++ src/GeneratePrimes/GeneratePrimes.js
@@ -0,0 +1,4 @@
+class GeneratePrimes {
+
+}
+export default GeneratePrimes;
---
+++ test/GeneratePrimes_test.js
@@ -0,0 +1,6 @@
+import assert from 'power-assert';
+import GeneratePrimes from '../src/GeneratePrimes/GeneratePrimes';
+
+describe('GeneratePrimes', () => {
+    it('should do something');
+});
