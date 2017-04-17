b7402511b33719e5af2a01b9db682943e024f2dc
k2works <kakimomokuri@gmail.com>
2017-04-17 11:31:33 +0900

    リファクタリング
    メソッドの抽出と説明変数のインライン化を実施する。

--- src/GeneratePrimes/GeneratePrimes.js
+++ src/GeneratePrimes/GeneratePrimes.js
@@ -1,15 +1,12 @@
 class GeneratePrimes {
     static generatePrimes(maxValue) {
-        let result;
-        let arraySize = maxValue + 1;
-        let isCrossed = Array.apply(null,new Array(arraySize)).map(function () { return true});
+        let isCrossed = Array.apply(null,new Array(maxValue + 1)).map(function () { return true });
         if (maxValue < 2) {
             return new Array(0);
         } else {
             this.initializeArrayOfInteger(isCrossed);
             this.crossOutMultiples(isCrossed);
-            result = this.putUncrossedIntegersIntoResult(isCrossed);
-            return result;
+            return this.putUncrossedIntegersIntoResult(isCrossed);
         }
     }
 
@@ -19,36 +16,47 @@ class GeneratePrimes {
     }
 
     static crossOutMultiples(isCrossed) {
-        let maxPrimeFactor = Math.sqrt(isCrossed.length) + 1;
+        let maxPrimeFactor = this.calcMaxPrimeFactor(isCrossed);
         for(let i = 2; i <= maxPrimeFactor; i++) {
-            if(!isCrossed[i]) {
-                for(let multiple = 2*i;
-                    multiple < isCrossed.length;
-                    multiple += i) {
-                    isCrossed[multiple] = true;
-                }
+            if(this.notCrossed(isCrossed, i)) {
+                this.crossOutMultiplesOf(i, isCrossed);
             }
         }
     }
 
     static putUncrossedIntegersIntoResult(isCrossed) {
-        // 見つけた素数の個数をカウント
+        let result = new Array(this.numberOfUncrossedIntegers(isCrossed));
+        for (let i = 0, j = 0; i < isCrossed.length; i++) {
+            if (this.notCrossed(isCrossed, i)) {
+                result[j++] = i;
+            }
+        }
+        return result;
+    }
+
+    static calcMaxPrimeFactor(isCrossed) {
+        return Math.sqrt(isCrossed.length) + 1;
+    }
+
+    static notCrossed(isCrossed, i) {
+        return isCrossed[i] === false;
+    }
+
+    static crossOutMultiplesOf(i, isCrossed) {
+        for (let multiple = 2 * i;
+             multiple < isCrossed.length;
+             multiple += i) {
+            isCrossed[multiple] = true;
+        }
+    }
+
+    static numberOfUncrossedIntegers(isCrossed) {
         let count = 0;
         for (let i = 0; i < isCrossed.length; i++) {
             if (!isCrossed[i]) {
-                count++; // カウントアップ
+                count++;
             }
         }
-
-        let primes = new Array(count);
-
-        // 素数の抜き出し
-        for (let i = 0, j = 0; i < isCrossed.length; i++) {
-            if (!isCrossed[i]) { // 素数であれば
-                primes[j++] = i;
-            }
-        }
-        return primes;
     }
 }
 export default GeneratePrimes;
