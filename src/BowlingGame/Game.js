a7fda1dea87a8e2662bdb2dd2e45eb8feffe272f
k2works <kakimomokuri@gmail.com>
2017-10-02 11:39:20 +0900

    フレーム別スコア計算メソッド新規作成 EFP-3
    フレーム別にスコアを集計できるようにする。
    １ゲーム１０フレームで１フレームあたり２回ボールを投げれる。
    ただし、１０フレームは最大３回ボールを投げれるので１ゲーム
    に投げられるボールの数は２１とする。

--- src/BowlingGame/Game.js
+++ src/BowlingGame/Game.js
@@ -1,18 +1,28 @@
 class Game {
     constructor() {
-        this.its_score = 0
+        this.itsScore = 0;
+        this.itsCurrent_throw = 0;
+        this.itsThrows = Array.apply(null, Array(21)).map(function() { return 0 });
     }
 
     score() {
-        return this.its_score;
+        return this.itsScore;
     }
 
     add(pins) {
-        this.its_score = pins;
+        this.itsThrows[this.itsCurrent_throw] = pins;
+        this.itsCurrent_throw += 1;
+        this.itsScore = pins;
     }
 
     scoreForFrame(theFrame) {
-        return 0;
+        let ball = 0;
+        let score = 0;
+        for (let currentFrame = 0; currentFrame < theFrame; currentFrame++) {
+            score += this.itsThrows[ball] + this.itsThrows[ball + 1];
+            ball += 2;
+        }
+        return score;
     }
 }
 export default Game;
